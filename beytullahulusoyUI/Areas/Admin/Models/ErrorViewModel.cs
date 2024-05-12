ef6c9c6270ce1c2e445c8a9b4ccd4e51d0c402ae
ahmet çiftçi <ciftciahmet70@hotmail.com>
2024-05-12 21:56:33 +0300

    Add project files.

---
+++ beytullahulusoyUI/Areas/Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace beytullahulusoyUI.Areas.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
